1b6a9d0dc46244ed3cda073b81df605343fedbb8
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-03-01 21:56:30 +0600

    2nd commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+   
+     
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    // console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => searchResult(data.data))
+}
+
+const searchResult = data => {
+    // console.log(data);
+    const displaySearch = document.getElementById('search-result');
+    displaySearch.textContent = '';
+     // error handaling
+     let error  = document.getElementById('error')
+     if(data.length == 0){
+       error.innerText = 'No Phone Found'
+       error.style.color = 'red'
+       error.style.textAlign = 'center'
+     }else{
+       error.textContent = '';
+     }
+    data.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="col">
+                <div class="card">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="text-center">${phone.phone_name}</h4>
+                        <h5 class="text-center">${phone.brand}</h5>
+                        <button onclick="moreInfo('${phone.slug}')" class="d-block m-auto btn btn-info text-white">Details</button>
+                    </div>
+                </div>
+         </div>      
+        `;
+        displaySearch.appendChild(div);
+    })
+}
+
+
+
